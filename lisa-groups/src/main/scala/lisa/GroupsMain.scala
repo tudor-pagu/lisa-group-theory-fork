0c983ebe940ed48deb31c0a53f4c9d6ed4bdde04
tudor <pagu.tudor@gmail.com>
2025-12-10 18:14:02 +0100

    binary op proof

--- lisa-groups/src/main/scala/lisa/GroupsMain.scala
+++ lisa-groups/src/main/scala/lisa/GroupsMain.scala
@@ -1,10 +1,18 @@
 package lisa.maths.GroupTheory
 
+import lisa.kernel.proof.*
+import lisa.kernel.proof.RunningTheoryJudgement._
+import lisa.maths.SetTheory.*
+import lisa.maths.SetTheory.Base.Singleton.*
+import lisa.maths.SetTheory.Base.Singleton
 import lisa.Main
 
 import lisa.automation.Congruence
 import lisa.automation.Substitution.{Apply => Substitute}
 import lisa.automation.Tableau
+import lisa.utils.prooflib.BasicStepTactic.RightForall
+import lisa.maths.GroupTheory.Groups.binaryOperation
+import lisa.maths.GroupTheory.Groups.binaryOperation
 
 object Example extends lisa.Main:
 
@@ -25,8 +33,8 @@ object Groups extends lisa.Main:
   val x = variable[Ind]
   val y = variable[Ind]
 
-  private val G = variable[Ind]
-  private val op = variable[Ind >>: Ind >>: Ind]
+  val G = variable[Ind]
+  val op = variable[Ind >>: Ind >>: Ind]
 
   val binaryOperation = DEF(λ(G, λ(op, 
     ∀(x, ∀(y, x ∈ G /\ y ∈ G ==> op(x)(y) ∈ G ))
@@ -39,4 +47,34 @@ object Groups extends lisa.Main:
 
 
   
+  /*
+   * The most simple group:
+   * G = {x}, op(x,x) = x
+   *
+   */
+object TrivialGroup extends lisa.Main:
+  private val x = variable[Ind]
+  private val y = variable[Ind]
+
+  private val e = constant[Ind]("e")
+
+  addSymbol(e)
+
+  private val G = singleton(e)
+  // x * y = e
+  private val star = DEF(λ(x, λ(y, e)))
+
+  // prove that this simple structure is indeed 
+  // a group
+  val trivialGroupIsGroup = Theorem(
+    () |- Groups.group(G)(star) 
+    ) {
+      val thm2 = have(star(x)(y) === e ) by Tautology.from(star.definition)
+      val thm3 = have(star(x)(y) ∈ G ) by Tautology.from(thm2, Singleton.membership of (x := e, y := star(x)(y)))
+      val thm4 = have(x ∈ G /\ y ∈ G  ==> star(x)(y) ∈ G ) by Tautology.from(thm3)
+      thenHave(∀(y, x ∈ G /\ y ∈ G  ==> star(x)(y) ∈ G )) by RightForall
+      val thm5 = thenHave(∀(x, ∀(y, x ∈ G /\ y ∈ G  ==> star(x)(y) ∈ G ))) by RightForall
+      val thm6 = have(binaryOperation(G)(star)) by Tautology.from(thm5, binaryOperation.definition of (Groups.G := G, Groups.op:= star))
+      val thm7 = have(Groups.group(G)(star)) by Tautology.from(thm6, Groups.group.definition of (Groups.G := G, Groups.op := star))
+    }
 
