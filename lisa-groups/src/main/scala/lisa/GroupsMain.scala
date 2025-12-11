66a1d388e1e37c4e2f25b5d966959ec8fa9053cd
tudor <pagu.tudor@gmail.com>
2025-12-11 03:28:31 +0100

    prove trivial group is group

--- lisa-groups/src/main/scala/lisa/GroupsMain.scala
+++ lisa-groups/src/main/scala/lisa/GroupsMain.scala
@@ -15,6 +15,7 @@ import lisa.maths.GroupTheory.Groups.binaryOperation
 import lisa.maths.GroupTheory.Groups.binaryOperation
 import lisa.maths.GroupTheory.Groups.isIdentityElement
 import lisa.maths.GroupTheory.Groups.isIdentityElement
+import lisa.maths.GroupTheory.Utils.equalityTransitivity
 
 object Utils extends lisa.Main:
   val x = variable[Ind]
@@ -87,6 +88,7 @@ object Groups extends lisa.Main:
 object TrivialGroup extends lisa.Main:
   private val x = variable[Ind]
   private val y = variable[Ind]
+  private val z = variable[Ind]
 
   private val e = constant[Ind]("e")
 
@@ -171,9 +173,38 @@ object TrivialGroup extends lisa.Main:
     val thm4 = thenHave( (∃(x, x ∈ G)) |- Groups.inverseElement(G)(star)) by Restate
     val thm6 = have(Groups.inverseElement(G)(star)) by Tautology.from(thm4, trivialGroupIsNotEmpty)
   }
-  //
-  // val trivialGroupIsGroup = Theorem(
-  //   () |- Groups.group(G)(star)
-  // ) {
-  //   have(Groups.group(G)(star)) by Tautology.from(trivialGroupHasBinaryOperation, Groups.group.definition of (Groups.G := G, Groups.op := star))
-  // }
+
+
+  val trivialGroupIsAssociative = Theorem(
+    () |- Groups.associativity(G)(star)
+    ) {
+      // DEF(λ(G, λ(op, ∀(x ∈ G, ∀(y ∈ G, ∀(z ∈ G, op(x)(op(y)(z)) === op(op(x)(y))(z)))))))
+      val thm1 = have(star(x)(star(y)(z)) === e) by Tautology.from(star.definition of (x:=x, y:=star(y)(z)))
+      val thm2 = have(star(star(x)(y))(z) === e) by Tautology.from(star.definition of (x:=star(x)(y), y:=z))
+      val thm3 = have(star(x)(star(y)(z)) === star(star(x)(y))(z) ) by Tautology.from(thm1, thm2, Utils.equalityTransitivity of (x:=star(x)(star(y)(z)), y:=e,z:=star(star(x)(y))(z)))
+
+      val thm4 = have(z ∈ G ==> (star(x)(star(y)(z)) === star(star(x)(y))(z)) ) by Tautology.from(thm3)
+      val thm5 = thenHave(∀(z, z ∈ G ==> (star(x)(star(y)(z)) === star(star(x)(y))(z)) )) by RightForall
+      val thm6 = have(∀(z ∈ G, star(x)(star(y)(z)) === star(star(x)(y))(z) )) by Restate.from(thm5)
+
+      val thm7 = have(y ∈ G ==> ∀(z ∈ G, star(x)(star(y)(z)) === star(star(x)(y))(z))) by Tautology.from(thm6)
+      val thm8 = thenHave(∀(y, y ∈ G ==> ∀(z ∈ G, star(x)(star(y)(z)) === star(star(x)(y))(z)))) by RightForall
+      val thm9 = have(∀(y ∈ G, ∀(z ∈ G, star(x)(star(y)(z)) === star(star(x)(y))(z)))) by Restate.from(thm8)
+
+      val thm10 = have(x ∈ G ==> ∀(y ∈ G, ∀(z ∈ G, star(x)(star(y)(z)) === star(star(x)(y))(z)))) by Tautology.from(thm9)
+      val thm11 = thenHave(∀(x, x ∈ G ==> ∀(y ∈ G, ∀(z ∈ G, star(x)(star(y)(z)) === star(star(x)(y))(z))))) by RightForall
+      val thm12 = have(∀(x ∈ G, ∀(y ∈ G, ∀(z ∈ G, star(x)(star(y)(z)) === star(star(x)(y))(z))))) by Restate.from(thm11)
+
+      val thm13 = have(Groups.associativity(G)(star)) by Tautology.from(thm12, Groups.associativity.definition of (Groups.G := G, Groups.op := star))
+    }
+
+  val trivialGroupIsGroup = Theorem(
+    () |- Groups.group(G)(star)
+  ) {
+    have(Groups.group(G)(star)) by Tautology.from(
+      trivialGroupHasBinaryOperation, 
+      trivialGroupIsAssociative,
+      trivialGroupHasInverse,
+      trivialGroupHasIdentityElement,
+      Groups.group.definition of (Groups.G := G, Groups.op := star))
+  }
