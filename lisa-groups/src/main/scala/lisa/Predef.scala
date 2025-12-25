1ade6208d7fcc0411ba3e1473c491db2be136d85
Cosmin Moldoveanu <C.A.Moldoveanu@student.tudelft.nl>
2025-12-25 15:57:35 +0200

    added isomorphism definition and notation

---
+++ lisa-groups/src/main/scala/lisa/Predef.scala
@@ -0,0 +1,15 @@
+package lisa.maths.GroupTheory
+
+object Predef {
+    export lisa.maths.GroupTheory.Symbols.*
+    export lisa.maths.GroupTheory.Groups
+    export lisa.maths.GroupTheory.Subgroups
+    export lisa.maths.GroupTheory.Cosets
+    export lisa.maths.GroupTheory.NormalSubgroups
+    export lisa.maths.GroupTheory.Lagrange
+    export lisa.maths.GroupTheory.QuotientGroup
+    export lisa.maths.GroupTheory.Homomorphisms
+    export lisa.maths.GroupTheory.Homomorphisms.{:::}
+    export lisa.maths.GroupTheory.Isomorphisms
+    export lisa.maths.GroupTheory.Isomorphisms.{::~, ≅}
+}
